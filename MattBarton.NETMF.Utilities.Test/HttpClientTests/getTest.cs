db032c8abfbbf2d46c639495c9b2f239a7604da2
Matt Barton <matt@mattbarton.org>
2012-06-20 14:50:24 +0100

    Add project structire

---
+++ MattBarton.NETMF.Utilities.Test/HttpClientTests/getTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace MattBarton.NETMF.Utilities.Test.HttpClientTests
+{
+	[TestFixture]
+	class getTest
+	{
+		[Test]
+		public void When_blahblahblah()
+		{
+			var target = new HttpClient();
+			var result = target.get("");
+
+			Assert.AreEqual(result, "response", "resonse is not correct");
+		}
+	}
+}
---
+++ MattBarton.NETMF.Utilities/HttpClient.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MattBarton.NETMF.Utilities
+{
+	public class HttpClient
+	{
+		public string get(string url, string arguments = "")
+		{
+			return "response";
+		}
+	}
+}
